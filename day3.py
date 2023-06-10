9dad817659eb3306f390c355e4c17b19b4c86e4e
dhuriyapawan <pawandhuirya.outlook.com>
2023-06-10 16:01:16 +0530

    day3 started

---
+++ day3.py
@@ -0,0 +1,6 @@
+##--Loops---
+number=float(input("which number do you want to check even or odd "))
+if number%2==0:
+    print(f"this is a even number",number )
+else:
+    print(f"this is a odd number",number )
