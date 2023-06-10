beedda5a1772a5b3237b8a0eb45e11bedc421717
dhuriyapawan <pawandhuirya.outlook.com>
2023-06-10 10:51:08 +0530

    Day 1 pyton coding

---
+++ # # Python practice Problem.py
@@ -0,0 +1,35 @@
+# # Python practice Problem
+# #1) User will input (3 ages).Find the oldest one
+
+# a =int(input(f"Enter the first ages:"))
+# b=int(input(f"Enter the secound ages:"))
+# c=int(input(f"Enter the third ages:"))
+
+# if a>b & a>c:
+#     print("the oddes is ",a)
+# elif b>c & b>a:
+#          print("the oddes is ",b)
+# else:
+#     print("the oddes is ",c)         
+    
+# --------while loop natural 10 nu
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+#----print pattern----
+# i=0
+# row=5
+# for i in range(1,row+1,1):
+#     for j in range(1,i+1,1):
+#         print(j,end=' ')
+#     print(' ')                  
+   
+
+# ---------input swap
+
+num1=[21,34,56,78]
+out=[]
+p=num1[1]
+print(p)
+
