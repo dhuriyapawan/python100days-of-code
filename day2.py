4547ed909b6bf4bb5dcbcb4e804ca7356d0b3ff5
dhuriyapawan <pawandhuirya.outlook.com>
2023-06-10 14:51:03 +0530

    day2 bmi

--- day2.py
+++ day2.py
@@ -1,3 +1,21 @@
-#---data types--silicing----
-print("hello"[4])
+# #---data types--silicing----
+# # string
+# print("hello"[4])
+# # ---type of data ---
+# num_char=len(input("enter the name:- "))
+# print(type(num_char))
+# num_str=str(num_char)
+# print(type(num_str))
+# ----data types---
+# two_digit=str(input("enter the two digit number: "))
+# a=two_digit[0]
+# b=two_digit[1]
+# result=int(a)+int(b)
+# print(result)
+# ----BMI Body index mask---
+height=float(input("enter your height in m: "))
+weight=int(input("enter your weight in kg: "))
+bmi=weight/(height**2)
+print(round(bmi,2))
+# ------1.75
 
