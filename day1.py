8293e5e1644288bac4fbea7ab2218712407fed85
dhuriyapawan <pawandhuirya.outlook.com>
2023-06-10 11:25:17 +0530

    changes

--- day1.py
+++ day1.py
@@ -1,3 +1,6 @@
 print("Hello world")
 #---------print in next line---
 print("Hello world!\nHello world!\nHello world! ")
+#---------string function---------
+name=input("What is your name:-")
+print(name)
