ee0c7254b896ddc48ab57ca4b70efd2c186a858a
dhuriyapawan <pawandhuirya.outlook.com>
2023-06-10 11:57:30 +0530

    day 1 completed

--- day1.py
+++ day1.py
@@ -1,6 +1,23 @@
-print("Hello world")
-#---------print in next line---
-print("Hello world!\nHello world!\nHello world! ")
-#---------string function---------
-name=input("What is your name:-")
-print(name)
+# print("Hello world")
+# #---------print in next line---
+# print("Hello world!\nHello world!\nHello world! ")
+# #---------string function---------
+# name=input("What is your name:-")
+# print(name)
+# #----len function----
+# # print(len (input("What is your name:-")))
+# # #-----swicth value store in variable----
+# a=input("a: ")
+# b=input("b: ")
+# c=a
+# a=b
+# b=c
+# print("a= "+ a)
+# print("b= "+b)
+
+#---project name generator---
+print("Create a name band genrator")
+city=input("Enter the city where u were born ")
+pet=input("enter the name of your pet ")
+print(f"the name generator is : "+city+" "+pet)
+ 
