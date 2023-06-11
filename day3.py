bb98a1a1842cd52af72becdf8a18d605262267f2
dhuriyapawan <pawandhuirya.outlook.com>
2023-06-11 14:58:59 +0530

    day3 started

--- day3.py
+++ day3.py
@@ -1,6 +1,19 @@
 ##--Loops---
-number=float(input("which number do you want to check even or odd "))
-if number%2==0:
-    print(f"this is a even number",number )
+# number=float(input("which number do you want to check even or odd "))
+# if number%2==0:
+#     print(f"this is a even number",number )
+# else:
+#     print(f"this is a odd number",number )
+#-----nested if else----
+height=int(input("Enter your height in cm: "))
+if height>=120:
+    print("you can ride")
+    age=int(input("enter the age in years"))
+    if age>=18:
+        print("pl pay $12")
+    elif age<18:
+        print("pl pay $7")
 else:
-    print(f"this is a odd number",number )
+    print("you cant ride")
+            
+    
