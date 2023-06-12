b1bd399fa2f67f20b6fb77b2f65ce0b4a98b647b
dhuriyapawan <pawandhuirya.outlook.com>
2023-06-12 06:05:05 +0530

    day 4 started

---
+++ day4.py
@@ -0,0 +1,12 @@
+###randomnization---
+# import random 
+# random_integer=random.randint (1,10)
+# print(random_integer)
+
+# ----heads and tail---
+import random
+random_side=random.randint(0,1)
+if random_side==1:
+    print("Heads")
+else:
+    print("tails")    
