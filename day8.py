728507a07dadc61266e5f92efdb3842525a002a8
dhuriyapawan <pawandhuirya.outlook.com>
2023-06-18 11:17:05 +0530

    day8 function satrted

--- day8.py
+++ day8.py
@@ -1,9 +1,17 @@
 #---Function 
 # Create a function called greet(). 
-def greet():
+# def greet():
 # Write 3 print statements inside the function.
- print("Hi")
- print("Heloo")
- print("chalo")
-# Call the greet() function and run your code.
-greet()
+#  print("Hi")
+#  print("Heloo")
+#  print("chalo")
+# # Call the greet() function and run your code.
+# greet()
+#----------- Function allows input
+def greet_with(name,location):
+ 
+ print(f"Hello {name}")
+
+ print(location)
+
+greet_with (name="pawan",location="nagpur")
